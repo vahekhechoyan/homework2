bb652487503d6810158c6cdc080e9017e0f3672c
vahekhechoyan <139881962+vahekhechoyan@users.noreply.github.com>
2024-03-18 18:31:45 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,51 @@
+# harc 1
+
+# def foo(num):
+#     res=[i for i in range(1,num-1)if num % i==0]
+#     return sum(res)
+
+# print(foo(24))
+
+
+
+# harc 2
+
+# x=[i for i in range(1,200+1) if i%2==0]
+# print(*x)
+
+
+# harc 3
+
+# def foo(n):
+#     return [(a, b, c) for a in range(1, n+1) for b in range(a, n+1) for c in range(b, n+1) if a**2 + b**2 == c**2]
+
+# print(foo(20))
+
+
+# harc 4
+
+# ls=[[3, 6, 9, 12], [4, 7, 10, 13], [5, 8, 11, 14], [6, 9, 12, 15]]
+
+# res=[[(i+j) for j in range(2,12,3) ] for i in range(1,5) ]
+# print(res)
+
+
+
+# harc 5
+
+
+
+
+
+
+# harc 6
+
+
+
+
+
+
+# harc7
+
+
+
